95d2b068e2a7ccdc957c5ce743e212b327c4597b
BuildTools <unconfigured@null.spigotmc.org>
2024-10-09 16:16:01 +0300

    Recipe exceptions

--- 2/recipe.py
+++ 2/recipe.py
@@ -9,8 +9,15 @@ class Recipe:
             for ing_name, ing_raw_weight, ing_cooked_weight, ing_cost in ingredients
         ]
 
-    def get_weight(self, portions = 1, raw : bool = False) -> int:
+    def get_weight(self, portions : int = 1, raw : bool = False) -> int:
+        if not isinstance(raw, bool): raise TypeError('Raw parameter is bool type')
+        if not isinstance(portions, int): raise TypeError('Portions parameter is a number')
+        if portions <= 0: raise ValueError('Portions count have to be more than 0')
+        
         return sum([ingredient.weight_raw if raw else ingredient.weight_cooked for ingredient in self.ingredients]) * portions
 
     def get_full_cost(self, portions = 1) -> int:
+        if not isinstance(portions, int): raise TypeError('Portions parameter is a number')
+        if portions <= 0: raise ValueError('Portions count have to be more than 0')
+
         return sum([ingredient.cost for ingredient in self.ingredients]) * portions
